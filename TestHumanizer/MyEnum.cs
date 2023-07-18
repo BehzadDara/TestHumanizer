aa013315f1d72fa0ab6a9d74a54a1535d1c7ccf2
behzad dara <Behzad.dara.99@gmail.com>
2023-07-19 00:31:31 +0330

    Add project files.

---
+++ TestHumanizer/MyEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace TestHumanizer
+{
+    public enum MyEnum
+    {
+        [Description("blue color description")]
+        Blue,
+        [Description("red color description")]
+        Red,
+        [Description("green color description")]
+        Green,
+        [Description("yellow color description")] 
+        Yellow
+    }
+}
---
+++ TestHumanizer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Humanizer;
+using TestHumanizer;
+
+foreach (var item in new List<int> { -2, -1, 0, 1, 2})
+{
+    var date = DateTime.Now.AddDays(item);
+    Console.WriteLine($"date {date} - Humanizer : {date.Humanize()}");
+}
+Console.WriteLine("-----------------------------------------------------");
+
+foreach (var item in new List<int> { -9012, 12345 })
+{
+    Console.WriteLine($"number {item} - Humanizer : {item.ToWords()}");
+}
+Console.WriteLine("-----------------------------------------------------");
+
+foreach (MyEnum item in Enum.GetValues(typeof(MyEnum)))
+{
+    Console.WriteLine($"enum {item} - Humanizer : {item.Humanize()}");
+}
+Console.WriteLine("-----------------------------------------------------");
+
+foreach (var item in new List<string> { "cat", "baby"})
+{
+    Console.WriteLine($"plural {item} - Humanizer : {item.Pluralize()}");
+}
+Console.WriteLine("-----------------------------------------------------");
+
+foreach (var item in new List<string> { "dogs", "butterflies"})
+{
+    Console.WriteLine($"singular {item} - Humanizer : {item.Singularize()}");
+}
+Console.WriteLine("-----------------------------------------------------");
+
+foreach (var item in new List<int> { 1, 3, 10 })
+{
+    Console.WriteLine($"ordinal {item} - Humanizer : {item.Ordinalize()}");
+}
+Console.WriteLine("-----------------------------------------------------");
